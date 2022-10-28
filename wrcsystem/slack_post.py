ff8b978e7ff6dad0c13d4363ac9763d55b7e977f
sougo0811 <sougo2813@outlook.jp>
2022-10-28 23:42:57 +0900

    トークン更新

--- wrcsystem/slack_post.py
+++ wrcsystem/slack_post.py
@@ -1,7 +1,7 @@
 import requests
 
 def slack_post(post_text):
-  TOKEN = 'xoxb-4159801049456-4295338328721-1XmmFo7PyPg2zz4ppRLfYC3j'
+  TOKEN = 'xoxb-4159801049456-4295338328721-lDdyKVS6byEMVS5RzRVzVDrY'
   CHANNEL = 'bot-space'
   
   url = "https://slack.com/api/chat.postMessage"
