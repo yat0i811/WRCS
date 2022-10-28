c79d0ad0f2a0f21cdf42d64bd5117f1e5aec94e4
sougo0811 <sougo2813@outlook.jp>
2022-10-29 00:00:21 +0900

    update token

--- wrcsystem/slack_post.py
+++ wrcsystem/slack_post.py
@@ -3,7 +3,7 @@ import requests
 def slack_post(post_text):
   TOKEN_a = '4159801049456'; TOKEN_b = '4295338328721'; TOKEN_c = 'bgMCh6xnOGTMrSUjw9Y14NFG'
   CHANNEL = 'bot-space'
-  TOKEN = 'xoxb'+ TOKEN_a + TOKEN_b + TOKEN_c
+  TOKEN = 'xoxb-'+ TOKEN_a + '-' + TOKEN_b + '-' + TOKEN_c
   
   url = "https://slack.com/api/chat.postMessage"
   headers = {"Authorization": "Bearer "+TOKEN}
